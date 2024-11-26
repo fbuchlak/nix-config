5e5efeea7ae74999702b001a6cc4acf05176a6ee
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2024-11-26 12:44:22 +0100

    feat: add firefox extensions

--- home/frederik/_common/optional/browsers/firefox.nix
+++ home/frederik/_common/optional/browsers/firefox.nix
@@ -5,16 +5,45 @@
     enable = true;
     policies = {
       AppAutoUpdate = false;
-      ExtensionUpdate = false;
       BackgroundAppUpdate = false;
 
       DisablePocket = true;
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
       DisableFirefoxAccounts = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
 
       OfferToSaveLogins = false;
       DontCheckDefaultBrowser = true;
+
+      ExtensionUpdate = false;
+      ExtensionSettings =
+        let
+          extension = shortId: uuid: {
+            name = uuid;
+            value = {
+              install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+              installation_mode = "normal_installed";
+            };
+          };
+        in
+        builtins.listToAttrs [
+          (extension "ublock-origin" "uBlock0@raymondhill.net")
+          (extension "privacy-badger17" "jid1-MnnxcxisBPnSXQ@jetpack")
+          (extension "istilldontcareaboutcookies" "idcac-pub@guus.ninja")
+
+          (extension "darkreader" "addon@darkreader.org")
+          (extension "tridactyl-vim" "tridactyl.vim@cmcaine.co.uk")
+
+          (extension "proton-pass" "78272b6fa58f4a1abaac99321d503a20@proton.me")
+        ];
+
     };
   };
 

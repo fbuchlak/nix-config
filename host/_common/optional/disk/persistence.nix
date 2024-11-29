993cdb2b02d9cada50594dc7b1c435b3839794d6
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2024-11-29 18:14:45 +0100

    feat: create persistent user home directories on activation

--- host/_common/optional/disk/persistence.nix
+++ host/_common/optional/disk/persistence.nix
@@ -1,4 +1,10 @@
-{ inputs, ... }:
+{
+  my,
+  lib,
+  inputs,
+  config,
+  ...
+}:
 {
 
   imports = [ inputs.impermanence.nixosModules.impermanence ];
@@ -13,5 +19,25 @@
   ];
   persist.cache.directories = [ "/var/cache" ];
   persist.logs.directories = [ "/var/log" ];
+
+  # Create user home directories for every persistent mount point
+  # so home manager can create it's own directories inside
+  system.activationScripts.create-persistent-user-home-directories.text =
+    with lib;
+    concatLines (
+      flatten (
+        builtins.map (
+          user:
+          (builtins.map (mountpoint: ''
+            if [ ! -d "${mountpoint}${user.home}" ] ; then
+                echo "Creating '${mountpoint}${user.home}' directory"
+                mkdir -p "${mountpoint}${user.home}"
+                echo "Setting owner of '${mountpoint}${user.home}' to '${user.name}:${user.group}'"
+                chown -R "${user.name}:${user.group}" "${mountpoint}${user.home}"
+            fi
+          '') (builtins.map (subvolume: subvolume.mnt) (attrValues my.vars.persistence)))
+        ) (builtins.filter (user: user.isNormalUser) (attrValues config.users.users))
+      )
+    );
 
 }

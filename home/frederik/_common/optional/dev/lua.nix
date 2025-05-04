613cd23d0ab73873bee9da02e8d867bcd76821b4
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2025-05-04 11:15:54 +0200

    feat: lua, python, julia

---
+++ home/frederik/_common/optional/dev/lua.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+let
+  lua51 = pkgs.lua5_1;
+  lua51bin = pkgs.writeShellScriptBin "lua5.1" ''
+    exec ${lua51}/bin/lua "$@"
+  '';
+in
+{
+
+  home.packages = with pkgs; [
+    lua
+    luarocks
+    lua51bin
+  ];
+
+}

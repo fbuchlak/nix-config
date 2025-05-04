1f53c8b2c14c3184c4309d8f766b23b96d0f1604
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2025-05-04 08:08:53 +0200

    feat: firefox

--- home/frederik/_common/optional/browsers/librewolf.nix
+++ home/frederik/_common/optional/browsers/librewolf.nix
@@ -1,40 +1,101 @@
 {
+  lib,
   pkgs,
   pkgs-system,
   inputs,
   ...
 }:
+let
+  firefox-addons = inputs.firefox-addons.packages."${pkgs.system}";
+  inherit (pkgs-system) custom-firefox-addons;
+in
 {
 
   programs.librewolf = {
     enable = true;
+    nativeMessagingHosts = with pkgs; [ tridactyl-native ];
     profiles = {
 
       frederik = {
-        extensions.packages =
-          with inputs.firefox-addons.packages."${pkgs.system}";
-          [
-            # ads/privacy
-            ublock-origin
-            canvasblocker
-            istilldontcareaboutcookies
-            # passwords
-            proton-pass
-            # youtube
-            sponsorblock
-            # ui
-            tridactyl
-            darkreader
-            # development
-            react-devtools
-            vue-js-devtools
-            angular-devtools
-          ]
-          ++ (with pkgs-system.custom-firefox-addons; [
-            autogram-na-statnych-weboch
-          ]);
+        id = lib.mkForce 0;
+
+        extensions = {
+          force = true;
+          packages =
+            with firefox-addons;
+            [
+              # ads/privacy
+              ublock-origin
+              canvasblocker
+              istilldontcareaboutcookies
+              # passwords
+              proton-pass
+              # youtube
+              sponsorblock
+              youtube-nonstop
+              return-youtube-dislikes
+              # other
+              tridactyl
+              darkreader
+            ]
+            ++ (with custom-firefox-addons; [
+              autogram-na-statnych-weboch
+            ]);
+          settings = {
+            "${firefox-addons.tridactyl.addonId}".settings = {
+              userconfig = {
+                theme = "midnight";
+              };
+            };
+          };
+        };
+
+        search = {
+          force = true;
+          default = "ddg";
+          privateDefault = "ddg";
+          engines = {
+            ddg = { };
+            google.metaData.hidden = true;
+          };
+        };
+
+        settings = {
+          # Enable extensions by default
+          "extensions.autoDisableScopes" = 0;
+          "extensions.update.enabled" = false;
+          # Privacy
+          "webgl.disabled" = false; # https://librewolf.net/docs/settings/#enable-webgl
+          "identity.fxaccounts.enabled" = false;
+          "privacy.resistFingerprinting.letterboxing" = true; # https://librewolf.net/docs/settings/#enable-letterboxing
+          # Sane defaults for browser
+          "middlemouse.paste" = false;
+          "browser.download.useDownloadDir" = false; # always ask where to save files
+          "browser.ctrlTab.sortByRecentlyUsed" = true;
+          "browser.tabs.closeWindowWithLastTab" = false;
+        };
+
       };
 
+    };
+
+    policies = {
+      AppAutoUpdate = false;
+      BackgroundAppUpdate = false;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisableFirefoxAccounts = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
+      OfferToSaveLogins = false;
+      DontCheckDefaultBrowser = true;
+      ExtensionUpdate = false;
     };
   };
 

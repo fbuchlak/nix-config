ff306a6a37ac6bd40cd4b94c7e0918a4cde205b9
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2025-06-22 13:14:21 +0200

    feat: configure navi, switch to tlrc

--- home/frederik/_common/optional/cli/man.nix
+++ home/frederik/_common/optional/cli/man.nix
@@ -1,15 +1,37 @@
-{ pkgs, ... }:
+{ lib, pkgs-system, ... }:
+let
+  inherit (pkgs-system) unstable;
+  tldr = unstable.tlrc;
+in
 {
 
-  home.packages = with pkgs; [ tldr ];
+  home.packages = [ tldr ];
 
   programs.navi = {
+    package = unstable.navi;
     enable = true;
     enableZshIntegration = true;
     enableBashIntegration = true;
+    settings = {
+      client.tealdeer = true;
+    };
   };
 
   persist.home.directories.data = [ "navi" ];
-  persist.cache.directories.cache = [ "tldr" ];
+  persist.cache.directories.cache = [ "tlrc" ];
+
+  programs.zsh.initContent = lib.mkOrder 1000 ''
+    function __zshrc_navi_load () {
+        eval $(${unstable.navi}/bin/navi widget zsh)
+        bindkey '^V' _navi_widget
+    }
+    zvm_after_init_commands+=(__zshrc_navi_load)
+  '';
+
+  services.tldr-update = {
+    enable = true;
+    period = "weekly";
+    package = tldr;
+  };
 
 }
